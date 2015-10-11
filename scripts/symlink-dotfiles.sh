e143ce5b081c24369f5586406c4ac546ed88f744
Marc Addeo <marcaddeo@gmail.com>
2015-10-11 09:44:24 -0700

    Add git configurations

--- scripts/symlink-dotfiles.sh
+++ scripts/symlink-dotfiles.sh
@@ -4,3 +4,7 @@ DOTFILES="$HOME/dotfiles"
 # Zsh
 ln -s $DOTFILES/.oh-my-zsh $HOME/.oh-my-zsh
 ln -s $DOTFILES/.zshrc $HOME/.zshrc
+
+# Git
+ln -s $DOTFILES/.gitconfig $HOME/.gitconfig
+ln -s $DOTFILES/.gitignore_global $HOME/.gitignore
