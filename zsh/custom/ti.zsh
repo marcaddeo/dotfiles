648e3f72e825c8f53864cd493f2fde109ea4e7cc
Marc Addeo <marcaddeo@gmail.com>
2015-10-12 10:01:11 -0400

    Add timetrap

---
+++ zsh/custom/ti.zsh
@@ -0,0 +1,27 @@
+function ti() {
+    timetrap $@
+
+    if [[ -n "$TMUX" ]]; then
+        tmux refresh-client -S
+    fi
+
+    if [[ ! -z $(cd $HOME/dotfiles; git diff --name-only timetrap/) ]]; then
+        pushd . >/dev/null
+        cd $HOME/dotfiles
+
+        if [[ ! -z $(cd $HOME/dotfiles; git diff --name-only | grep -v timetrap/) ]]; then
+            echo "Cowardly refusing to auto-commit time sheets because the repository is dirty!"
+        else
+            echo "Pushing to Github..."
+            git checkout master >/dev/null 2>&1
+            git add timetrap >/dev/null 2>&1
+            git commit -m "Update time sheets" >/dev/null 2>&1
+            git push origin master >/dev/null 2>&1
+        fi
+
+        popd >/dev/null
+    fi
+}
+
+alias tio="timetrap out"
+alias tid="timetrap display"
