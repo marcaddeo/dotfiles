a75d4d6b87cc98b3c8d5ff3cd42a47f568cb6502
Marc Addeo <marcaddeo@gmail.com>
2015-10-12 19:16:53 -0700

    Remove the tasks symlink from my home directory and explicity point to the dotfiles directory

--- zsh/custom/t.zsh
+++ zsh/custom/t.zsh
@@ -1,7 +1,7 @@
 # Refresh the tmux display every time we run a t command so that our task count
 # in the status bar is updated immediately
 function t() {
-    python $HOME/dotfiles/script/t --task-dir ~/tasks --list tasks $@
+    python $HOME/dotfiles/script/t --task-dir ~/dotfiles/tasks --list tasks $@
 
     if [[ -n "$TMUX" ]]; then
         tmux refresh-client -S
