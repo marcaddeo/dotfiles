8b0a053537865f1e19d0f9d0a8f100a725aefc12
Marc Addeo <marcaddeo@gmail.com>
2015-10-12 21:19:05 -0400

    Add custom git aliases

---
+++ zsh/custom/git.zsh
@@ -0,0 +1,2 @@
+# Override the glg alias to be more informative
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
