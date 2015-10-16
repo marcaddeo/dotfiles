90ca45c08e05f040d7b1cbcbfc6b91e8a803a9a1
Marc Addeo <marcaddeo@gmail.com>
2015-10-15 21:02:39 -0400

    Update the t function and add some shortcuts with fzf

--- zsh/custom/t.zsh
+++ zsh/custom/t.zsh
@@ -1,8 +1,12 @@
-# Refresh the tmux display every time we run a t command so that our task count
-# in the status bar is updated immediately
-function t() {
+function _t() {
     python $HOME/dotfiles/script/t --task-dir ~/dotfiles/tasks --list tasks $@
+}
 
+function _pick_task {
+    echo $(_t | fzf-tmux -d40% --ansi | perl -pe 's~^#\d+\s+- (.*?)\s+-.*~\1~')
+}
+
+function _refresh_and_push_tasks {
     if [[ -n "$TMUX" ]]; then
         tmux refresh-client -S
     fi
@@ -21,6 +25,38 @@ function t() {
             git push origin master >/dev/null 2>&1
         fi
 
-        popd >/dev/null
+        popd >/dev/null 2>&1
     fi
+}
+
+function t() {
+    _t $@
+    _refresh_and_push_tasks
+}
+
+function tf {
+    _t -f $(_pick_task)
+    _refresh_and_push_tasks
+}
+
+function tr {
+    _t -r $(_pick_task)
+    _refresh_and_push_tasks
+}
+
+function te {
+    ID=$(_pick_task)
+
+    if [[ ! -z $ID ]]; then
+        NOTE=$(_t -n | grep "^#\d\+\s\+- $ID" | perl -pe "s~^#\d+\s+- $ID\s+- (.*)~\1~")
+        TMP=$(mktemp -t "t-$ID")
+
+        echo $NOTE > $TMP && ${EDITOR:-vim} $TMP
+
+        NOTE=$(<$TMP)
+
+        _t -e $ID "$NOTE"
+    fi
+
+    _refresh_and_push_tasks
 }
