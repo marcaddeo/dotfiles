377a21b948090a1248432b00af7f1b130e42b311
Marc Addeo <marcaddeo@gmail.com>
2015-10-24 16:29:07 -0400

    Add a gcd alias that switches to either development or develop branches if they are available

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -1,3 +1,13 @@
 # Override the glg alias to be more informative
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 alias gc="git commit -e -v"
+
+function gcd {
+    if [[ ! -z $(git branch | grep development) ]]; then
+        git checkout development
+    elif [[ ! -z $(git branch | grep develop) ]]; then
+        git checkout develop
+    else
+        echo "Could not find a \`development\` or \`develop\` branch"
+    fi
+}
