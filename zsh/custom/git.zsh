24e9b7b6fe66daaa54bfc7b8f7daac405f9a4f9a
Marc Addeo <marcaddeo@gmail.com>
2015-10-24 16:28:42 -0400

    Override the gc alias to always open vim to edit the commit message

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -1,2 +1,3 @@
 # Override the glg alias to be more informative
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias gc="git commit -e -v"
