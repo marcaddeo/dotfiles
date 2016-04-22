ecb21af144f49af7950dbc6602e98be749964d11
Marc Addeo <marcaddeo@gmail.com>
2016-04-22 11:07:15 -0400

    Show signing status of commits in the glg output

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -1,5 +1,5 @@
 # Override the glg alias to be more informative
-alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias glg="git log --graph --pretty=format:'%Cred%h%Creset %C(bold yellow)[%G?]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset %C(bold blue)' --abbrev-commit --date=relative"
 alias gc="git commit -e -v"
 
 function gcd {
