bfb64b17df9953ab21aefeb4f0007bb1f395d61d
Marc Addeo <marcaddeo@gmail.com>
2023-04-26 09:54:31 -0400

    Add aucmnd for auto close nvimtree

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
+-- Auto close nvim-tree on quit.
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  pattern = "NvimTree_*",
+  callback = function()
+    local layout = vim.api.nvim_call_function("winlayout", {})
+    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+  end
+})
+
 vim.cmd [[
 augroup vagrant
   autocmd!
@@ -5,7 +15,6 @@ augroup vagrant
 augroup END
 
 autocmd filetype crontab setlocal nobackup nowritebackup
-
 " Drupal files
 autocmd BufRead,BufNewFile *.module set filetype=php
 autocmd BufRead,BufNewFile *.install set filetype=php
@@ -29,6 +38,7 @@ autocmd FileType gitcommit setlocal spell
 
 " Auto commit wiki files on save
 autocmd BufWritePost ~/dotfiles/wiki/* call jobstart(["bash", "-c", "~/dotfiles/wiki/bin/commit " . expand("%")], {})
+
 
 augroup PHP
   autocmd!
