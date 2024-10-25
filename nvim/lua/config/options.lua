115a243c2cbabeb294b0dc5761653a3ab5419557
Marc Addeo <marc_addeo@brown.edu>
2024-10-25 14:20:21 -0400

    Add listchars

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,12 @@
 local opt = vim.opt
 
 opt.wrap = true -- Enable line wrap @TODO consider removing <leader>uw
+
+opt.list = true -- Enable list chars to make incorrect characters stand out
+-- Trying out a new tab character than i normally use, usually use:
+--   tab = "▸ ",
+-- @see https://vi.stackexchange.com/questions/422/how-can-i-display-tabs-as-characters
+opt.listchars = {
+  tab = "⍿·",
+  eol = "¬",
+}
