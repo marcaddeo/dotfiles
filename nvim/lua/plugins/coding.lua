78c2b07192fdea1c7b99e9b1f5f9bfbcbc86054e
Marc Addeo <marcaddeo@gmail.com>
2024-11-15 11:11:07 -0500

    Fix snippet tab completion jumping cursor around
    If you enter a snippet, but didn't tab to each of it's placeholders and
    then went and tried to insert a literal tab somewhere else in the file
    (after exiting insert mode from the original snippet) it would jump your
    cursor back to any leftover placeholders from the snippet. This fixes
    that by "unlinking" the current snippet when leaving insert mode.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -7,6 +7,27 @@ return {
     dependencies = {
       "rafamadriz/friendly-snippets",
       config = function()
+        local luasnip = require("luasnip")
+
+        vim.api.nvim_create_autocmd("ModeChanged", {
+          group = vim.api.nvim_create_augroup("UnlinkLuaSnipSnippetOnModeChange", {
+            clear = true,
+          }),
+          pattern = { "s:n", "i:*" },
+          desc = "Forget the current snippet when leaving the insert mode",
+          callback = function(evt)
+            -- If we have n active nodes, n - 1 will still remain after a `unlink_current()` call.
+            -- We unlink all of them by wrapping the calls in a loop.
+            while true do
+              if luasnip.session and luasnip.session.current_nodes[evt.buf] and not luasnip.session.jump_active then
+                luasnip.unlink_current()
+              else
+                break
+              end
+            end
+          end,
+        })
+
         require("luasnip.loaders.from_vscode").lazy_load()
         require("luasnip.loaders.from_vscode").lazy_load({
           paths = vim.fn.stdpath("config") .. "/snippets/",
