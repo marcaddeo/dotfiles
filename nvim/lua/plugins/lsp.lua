1d0ff76692b2c75cd1dbacedc02a041a12a95c72
Marc Addeo <marc_addeo@brown.edu>
2025-03-21 15:00:52 -0400

    Add just-lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,14 +3,27 @@ return {
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        tailwindcss = {
-          filetypes_include = { "jinja" },
-        },
-      },
-    },
+    opts = function(_, opts)
+      local lsp = require("lspconfig")
+      local configs = require("lspconfig.configs")
+
+      if not configs.just_lsp then
+        configs.just_lsp = {
+          default_config = {
+            cmd = { "/Users/maddeo/.cargo/bin/just-lsp" },
+            filetypes = { "just" },
+            root_dir = function(fname)
+              return lsp.util.find_git_ancestor(fname)
+            end,
+            settings = {},
+          },
+        }
+      end
+
+      lsp.just_lsp.setup({})
+
+      table.insert(opts.servers.tailwindcss.filetypes_include, "jinja")
+    end,
   },
   -- Custom additions.
 }
