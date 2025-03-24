dd17b8272a602474c02799e594dde5858de89d0d
Marc Addeo <marcaddeo@gmail.com>
2025-03-24 09:27:28 -0400

    Add tailwind-tools

---
+++ nvim/lua/plugins/lang/tailwindcss.lua
@@ -0,0 +1,15 @@
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  opts = {
+    conceal = {
+      enabled = true,
+    },
+  },
+}
